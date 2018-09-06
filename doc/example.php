552a660cb88d0322a5a357fffd3a15cae5ae77c8
Christian Scheb <info@stylemotion.de>
2018-09-06 23:33:07 +0200

    Update usage example

--- doc/example.php
+++ doc/example.php
@@ -2,13 +2,18 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
+use Scheb\InMemoryDataStorage\DataRepositoryBuilder;
+use Scheb\InMemoryDataStorage\DataStorage\ArrayDataStorage;
+use function \Scheb\InMemoryDataStorage\Repository\compare;
+
 $foo = 'I am foo';
 $bar = 'I am bar';
 
-$repositoryBuilder = new \Scheb\InMemoryDataStorage\DataRepositoryBuilder();
+$repositoryBuilder = new DataRepositoryBuilder();
 $repository = $repositoryBuilder
-//  ->setDataStorage(new \Scheb\InMemoryDataStorage\DataStorage\ArrayDataStorage())
+//  ->setDataStorage(new ArrayDataStorage())
     ->build();
+
 
 // Simple CRUD
 $repository->addItem($foo);
@@ -23,3 +28,29 @@ $repository->getNamedItem('foo'); // returns $foo
 $repository->replaceNamedItem('foo', $bar);
 $repository->getNamedItem('foo'); // returns $bar
 $repository->removeNamedItem('foo');
+
+
+// Advanced get
+$repository->getAllItemsByCriteria(['property' => 'value']);
+// $repository->getOneItemByCriteria(...); // The same, but only one item is retrieved
+
+// Advanced update
+$repository->updateAllItemsByCriteria(
+    ['property' => 'value'], // Match criteria
+    ['property' => 'newValue', 'otherProperty' => 42] // Property updates
+);
+// $repository->updateOneByCriteria(...); // The same, but only one item is updated
+
+// Advanced remove
+$repository->removeAllItemsByCriteria(['property' => 'value']);
+// $repository->removeOneItemByCriteria(...); // The same, but only one item is removed
+
+
+// Comparision functions in matching criteria
+$repository->getAllItemsByCriteria(['property' => compare()->notNull()]);
+$repository->getAllItemsByCriteria(['property' => compare()->lessThan(3)]);
+$repository->getAllItemsByCriteria(['property' => compare()->between(1, 3)]);
+$repository->getAllItemsByCriteria(['property' => compare()->isInArray([1, 2, 3])]);
+$repository->getAllItemsByCriteria(['property' => compare()->arrayContains('arrayElement')]);
+$repository->getAllItemsByCriteria(['property' => compare()->dateGreaterThan(new \DateTime('2018-01-01'))]);
+// ... and many more, see Scheb\InMemoryDataStorage\Comparison
